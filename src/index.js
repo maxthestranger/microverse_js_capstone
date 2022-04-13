7b1a7b1250f82436e22fa833e659b55a1d0fc250
Lucas Shangrita <oustanilokman@gmail.com>
2022-04-13 13:06:52 +0300

    Add webpack's images loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import Icon from '../images/logo.svg';
+
+const logo = document.querySelector('.logo');
+const myIcon = new Image();
+myIcon.src = Icon;
+
+logo.appendChild(myIcon);
